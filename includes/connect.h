0674ce364c81ed564860acc0b2dbe27d4f2318c7
Xavier Morera <xmorera@joviat.cat>
2016-11-03 18:58:57 +0100

    actualització usuari base de dades

--- includes/connect.h
+++ includes/connect.h
@@ -1,5 +1,5 @@
 <?php
-	$mysqli = mysqli_connect("localhost", "raul", "Raul101$&", "projectxray2");
+	$mysqli = mysqli_connect("localhost", "root", "sdaw", "projectxray2");
 	mysqli_set_charset($mysqli, "utf8");
 	if (mysqli_connect_errno()){
 	die ('Consulta invalida:error de conexió' . mysqli_connect_error());
