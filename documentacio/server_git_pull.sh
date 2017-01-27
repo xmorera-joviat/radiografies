130c8ed96330c3dcd0ad6f7f374f22fb759ce765
Sergey <smusyus@gmail.com>
2017-01-27 17:25:04 +0100

    pujada de documentacio

---
+++ documentacio/server_git_pull.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##################################################
+# script per sincronitzar el servidor amb git.
+# es crea un cron per executar l'script.
+# 
+
+# anem al directori que volem sincronitzar
+cd /var/www/html/projectx/
+
+# sincronitzem amb el repositori de la branca principal
+git pull
+
+##################################################
+# entrada del cron per que s'actualitzi cada dia a 
+# les 5 del matí.
+#########
+# 0 5 * * * /root/scripts/server_git_pull.sh >> /dev/null 2>&1
+#
+# s'usa crontab -e per introduir aquesta entrada
