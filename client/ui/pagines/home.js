f3c54c5fe9cffa51be548f254cef49bfabbbe037
martiguba <martigual@hotmail.com>
2017-02-09 16:59:44 +0100

    Tabular funciona

--- client/ui/pagines/home.js
+++ client/ui/pagines/home.js
@@ -1,19 +1,2 @@
 Meteor.subscribe('posicions');
-TabularTables = {};
 
-Meteor.isClient && Template.registerHelper('TabularTables', TabularTables);
-
-TabularTables.Posicions = new Tabular.Table({
-  name: "Posicions",
-  collection: Posicions,
-  columns: [
-    {data: "id", title: "Id"},
-    {data: "zona", title: "Zona"},
-    {data: "os", title: "Os"},
-    {data: "posicio", title: "Posicion"},
-    {data: "centratge", title: "Centratge"},
-    {
-      tmpl: Meteor.isClient && Template.PosicionsActionBtns, class: "col-md-1"
-    }
-  ]
-});
