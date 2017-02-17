e3b59e8832f92edee46a46804ac17e222e77b017
Sergey <smusyus@gmail.com>
2017-02-17 23:50:48 +0100

    menu nova estructura

--- imports/ui/components/nav.js
+++ imports/ui/components/nav.js
@@ -3,24 +3,5 @@ import './nav.html';
 
 Template.nav.events({
 
-'click #btn-menuOpen':function(){
-var  mySidenav = document.getElementById("mySidenav");
-var  overlayBg = document.getElementById("myOverlay");
-
-  if (mySidenav.style.display === 'block') {
-      mySidenav.style.display = 'none';
-      overlayBg.style.display = "none";
-  } else {
-      mySidenav.style.display = 'block';
-      overlayBg.style.display = "block";
-  }
-},
-'click #myOverlay':function(){
-
-  var  mySidenav = document.getElementById("mySidenav");
-  var  overlayBg = document.getElementById("myOverlay");
-  mySidenav.style.display = "none";
-  overlayBg.style.display = "none";
-}
 
 });
--- imports/ui/layouts/baseLayout.js
+++ imports/ui/layouts/baseLayout.js
@@ -1,3 +1,9 @@
 import './baseLayout.html';
 import '../components/menu.js';
 import '../components/nav.js';
+
+Template.baseLayout.events({
+  "click [data-toggle='offcanvas']": function(){
+    Modal.show('menu');
+  }
+});
