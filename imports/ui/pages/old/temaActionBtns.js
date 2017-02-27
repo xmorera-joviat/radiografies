23725c6d5d3df729a3f3ecf870159493a5cc63f6
Unknown <toni_salvador_4@hotmail.com>
2017-02-27 19:02:08 +0100

    Crear branca + inici creacio metodes llistat temes

--- imports/ui/pages/old/temaActionBtns.js
+++ imports/ui/pages/old/temaActionBtns.js
@@ -1,10 +1,16 @@
 /**
  * Created by Toni Salvador i Martí Gual
  */
-Template.temaActionBtns.events({
+ Template.temesEditar.events({
+ 	'click #editar': function(event, template){
+ 		var obj = {};
 
-	'click #btnUpdate': function(){
-	},
-	'click #btnRemove': function(){
-	}
-});
+     var id         = $('input[name="modalId"]').val();
+     obj.nom        = $('input[name="modalNom"]').val();
+     obj.descripcio = $('input[name="modalDescripcio"]').val();
+
+     Meteor.call('editarTema', obj, id);
+     Modal.hide();
+   }
+ 	}
+ });
