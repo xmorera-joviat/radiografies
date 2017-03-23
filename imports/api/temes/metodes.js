3a1aae0f095a18cbc137bf4f2de665df1e068031
martiguba <martigual@hotmail.com>
2017-03-23 19:24:50 +0100

    asdadas

--- imports/api/temes/metodes.js
+++ imports/api/temes/metodes.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-import { Meteor } from "meteor/meteor";
-import { ValidatedMethod } from "meteor/mdg:validated-method";
-import { SimpleSchema } from "meteor/aldeed:simple-schema";
-import { Tema } from "../old/collections/tema.js";
-
-export const crearTema = new ValidatedMethod({
-  name: "tema.add",
-  validate: new SimpleSchema({
- 	  nom: { type: String},
- 	  descripcio: { type: String},
- 	  usuari: { type: String}
-    }).validator(),
-  run({nom,descripcio,usuari}) {
-    if(!this.userId){
-		throw new Meteor.Error("tema.add.unauthorized",
-        "Permís denegat. Cal estar identificat");
-	}
-	return Tema.insert({nom:nom,descripcio:descripcio,usuari:usuari});
-  }
-});
-export const editarTema = new ValidatedMethod({
-	name: "tema.edit",
-	validate: new SimpleSchema({
- 	id: { type: String},
- 	nom: { type: String},
- 	descripcio: { type: String},
- 	usuari: { type: String}
- 	}).validator(),
- 	run({id,nom,descripcio,usuari}) {
- 		if(!this.userId){
-				throw new Meteor.Error("tema.edit.unauthorized",
- 			"Permís denegat. Cal estar identificat");
- 		}
- 	return Tema.update({_id: id},{$set:{nom:nom,descripcio:descripcio,usuari:usuari}});
- 	}
-});
-export const eliminarTema = new ValidatedMethod({
-  name: "tema.remove",
-=======
 /* Creat per Toni Salvador */
 import { Meteor } from "meteor/meteor";
 import { ValidatedMethod } from "meteor/mdg:validated-method";
@@ -49,6 +9,59 @@ export const editarTema = new ValidatedMethod({
     validate: TemaSchema.validator(),
     run({
         id,
+        nom,
+        descripcio,
+        usuari
+    }) {
+        if (!this.userId) {
+            throw new Meteor.Error("Temes.edit.unauthorized",
+                "Permís denegat. Cal estar identificat");
+        }
+        return Temes.update({
+            _id: id
+        }, {
+            $set: {
+                nom: nom,
+                descripcio: descripcio,
+                usuari: usuari
+            }
+        });
+    }
+});
+
+export const crearTema = new ValidatedMethod({
+  name: "Temes.add",
+  validate: new SimpleSchema({
+      nom: {type: String},
+      descripcio: {type: String},
+      usuari: {type: String}
+  }).validator(),
+  run({
+      nom,
+      descripcio,
+      usuari
+    }) {
+    if(!this.userId){
+    throw new Meteor.Error("Temes.add.unauthorized",
+        "Permís denegat. Cal estar identificat");
+  }
+  return Temes.insert({nom:nom,descripcio:descripcio});
+  }
+});
+export const eliminarTema = new ValidatedMethod({
+  name: "Temes.remove",
+  validate: new SimpleSchema({
+      id: { type: String}
+  }).validator(),
+  run({id}) {
+    if(!this.userId){
+    throw new Meteor.Error("Temes.remove.unauthorized",
+        "Permís denegat. Cal estar identificat");
+  }
+  return Temes.remove({_id: id});
+  }
+});
+d,
         nom,
         descripcio,
         usuari
--- imports/api/temes/temes.js
+++ imports/api/temes/temes.js
@@ -1,32 +1,5 @@
 import { Tabular } from 'meteor/aldeed:tabular';
 import { Template } from 'meteor/templating';
-<<<<<<< HEAD
-import moment from 'moment';
-import { Meteor } from 'meteor/meteor';
-import { Tema } from '../old/collections/tema.js';
-TabularTables = {};
-
-Meteor.isClient && Template.registerHelper('TabularTables', TabularTables);
-
-TabularTables.Tema = new Tabular.Table({
-	name: "Tema",
-	collection: Tema,
-	columns: [
- 		{data: "_id", title: "id"},
- 		{data: "nom", title: "Nom"},
- 		{data: "descripcio", title: "Descripcio"},
- 		{data: "usuari", title: "idUsuari"},
- 		{title:"Usuari",mpl: Meteor.isClient && Template.usuariNom,
-		tmplContext(usuariNom) {
-			    return {
-			      item: usuariNom,
-			      column: 'title:aixonofuncioina'
-			    };
-		}
-	 	}
- 	]	
-});
-=======
 import { Meteor } from 'meteor/meteor';
 import moment from 'moment';
 
@@ -94,4 +67,3 @@ new Tabular.Table({
    {data: "usuari", title: "Usuari"}
    ]
 });
->>>>>>> master
